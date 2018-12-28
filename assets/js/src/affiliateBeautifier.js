1f6e27639746b5cb852eb600b84e088454d1e37a
Jens Törnell <jens.tornell@gmail.com>
2018-12-28 23:04:38 +0100

    New ES6 version

---
+++ assets/js/src/affiliateBeautifier.js
@@ -0,0 +1,44 @@
+class AffiliateBeautifier {
+  constructor(options) {
+    this.o = Object.assign({}, this.defaults(), options);
+
+    document.addEventListener('DOMContentLoaded', () => {
+      this.switchAll();
+      this.eventClick();
+    });
+  }
+
+  defaults() {
+    return {
+      selector: '[data-href]'
+    };
+  }
+
+  eventClick() {
+    ['mousedown', 'touchstart'].forEach((event) => {
+      document.querySelectorAll(this.o.selector).forEach((element) => {
+        element.addEventListener(event, (e) => {
+          this.switch(e.target);
+        });
+      });
+    });
+  }
+
+  switchAll() {
+    document.querySelectorAll(this.o.selector).forEach((el) => {
+      this.switch(el);
+    });
+  }
+
+  switch(el) {
+    let href = el.getAttribute('href');
+    let data_href = el.dataset.href;
+
+    el.setAttribute('href', data_href);
+    el.dataset.href = href;
+  }
+}
+
+function affiliateBeautifier(args) {
+  new AffiliateBeautifier(args);
+}
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+let gulp = require("gulp");
+//let sass = require("gulp-sass");
+//let autoprefixer = require("gulp-autoprefixer");
+let concat = require("gulp-concat");
+let uglify = require('gulp-uglify-es').default;
+//let csso = require('gulp-csso');
+let rename = require('gulp-rename');
+
+var paths = {
+    //styles: {
+      //  src: "assets/css/src/**/*.scss",
+        //dest: "assets/css/dist"
+//},
+    scripts: {
+        src: "assets/js/src/**/*.js",
+        dest: "assets/js/dist"
+    }
+};
+
+/*function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(concat('style.scss'))
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(autoprefixer())
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(csso())
+            .pipe(rename({extname: '.min.css'}))
+            .pipe(gulp.dest(paths.styles.dest))
+    );
+}*/
+
+function script() {
+    return (
+        gulp
+            .src(paths.scripts.src)
+            .pipe(concat('script.js'))
+            .pipe(gulp.dest(paths.scripts.dest))
+            .pipe(uglify())
+            .pipe(rename({extname: '.min.js'}))
+            .pipe(gulp.dest(paths.scripts.dest))
+    );
+}
+
+function watch(){
+    //gulp.watch(paths.styles.src, style);
+    gulp.watch(paths.scripts.src, script);
+}
+
+//exports.style = style;
+exports.script = script;
+exports.watch = watch;
